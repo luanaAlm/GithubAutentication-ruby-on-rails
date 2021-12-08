5c2ce3d0badc0a1ac2f4035c235b159fdadee2c6
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-12-08 13:36:24 -0300

    Criando ou logando usuário via Github

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,7 +1,7 @@
 #espera a chamada vina do git hub
-class callbacksController < Devise::OmniauthCallbacksControler
+class CallbacksController < Devise::OmniauthCallbacksController
     def github
-        @user = User.from_omniauth(request.env['omniauth.auth'])
-        sing_in_and_redirect @user
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in_and_redirect @user
     end
-end
+  end
